755bd6852e013b0ef762a6430438c4c3fc11a825
Marcos Fernandes <bark@cyclop.dog>
2023-11-30 13:32:00 -0300

    feat: :sparkles: Wrote game loop, added a blue circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,7 +4,12 @@ int main() {
   int width = 350;
   InitWindow(width, 200, "A Very Fancy Window");
 
-  while (true) {
-    
+  while (WindowShouldClose() == false) {
+    BeginDrawing();
+    ClearBackground(RED);
+
+    DrawCircle(175, 100, 25.0, BLUE);
+
+    EndDrawing();
   }
 }
