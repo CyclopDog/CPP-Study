c885fa38e6e5f93ae7d35957e0521484245c220c
Marcos Fernandes <bark@cyclop.dog>
2023-12-01 00:23:54 -0300

    feat: :sparkles: adds left and right movement to the circle within boundaries

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,14 +1,27 @@
 #include "raylib.h"
 
 int main() {
-  int width = 350;
-  InitWindow(width, 200, "A Very Fancy Window");
+  int width{350};
+  int height{200};
+  InitWindow(width, height, "A Very Fancy Window");
 
+  int circle_x{175};
+  int circle_y{100};
+
+  SetTargetFPS(60);
   while (WindowShouldClose() == false) {
     BeginDrawing();
-    ClearBackground(RED);
+    ClearBackground(WHITE);
 
-    DrawCircle(175, 100, 25.0, BLUE);
+    DrawCircle(circle_x, circle_y, 25.0, BLUE);
+
+    if (IsKeyDown(KEY_D) && circle_x < 350) {
+      circle_x = circle_x + 10;
+    }
+
+    if (IsKeyDown(KEY_A) && circle_x > 0) {
+      circle_x = circle_x - 10;
+    }
 
     EndDrawing();
   }
