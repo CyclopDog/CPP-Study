cc8b54d8b48dfa878ab019656f0fdaf362f42129
Marcos Fernandes <bark@cyclop.dog>
2023-12-01 00:50:01 -0300

    feat: :sparkles: adds collisions to the circle and square

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,9 +7,21 @@ int main() {
 
   int circle_x{200};
   int circle_y{200};
+  int circle_radius{25};
+
+  int l_circle_x{circle_x - circle_radius};
+  int r_circle_x{circle_x + circle_radius};
+  int u_circle_y{circle_y - circle_radius};
+  int b_circle_y{circle_y + circle_radius};
 
   int axe_x{400};
   int axe_y{0};
+  int axe_length{50};
+
+  int l_axe_x{axe_x};
+  int r_axe_x{axe_x + axe_length};
+  int u_axe_y{axe_y};
+  int b_axe_y{axe_y + axe_length};
 
   int direction{10};
 
@@ -18,15 +30,15 @@ int main() {
     BeginDrawing();
     ClearBackground(WHITE);
 
-    DrawCircle(circle_x, circle_y, 25.0, BLUE);
-    DrawRectangle(axe_x, axe_y, 50, 50, RED);
+    DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+    DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
     axe_y += direction;
-    if (axe_y > 450 || axe_y < 0) {
+    if (axe_y > height || axe_y < 0) {
       direction = -direction;
     }
 
-    if (IsKeyDown(KEY_D) && circle_x < 350) {
+    if (IsKeyDown(KEY_D) && circle_x < width) {
       circle_x += 10;
     }
 
